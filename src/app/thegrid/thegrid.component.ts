19b6d73cabb278ee3892eba3ada0616d401fcb99
hamiltoncameron7 <hamiltoncameron7@gmail.com>
2022-12-03 20:06:28 -0500

    Cleaned up code and added comments

--- src/app/thegrid/thegrid.component.ts
+++ src/app/thegrid/thegrid.component.ts
@@ -6,69 +6,78 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./thegrid.component.css']
 })
 export class ThegridComponent implements OnInit {
-  //enable a messageEvent function to be triggered with EventEmitter, so we can refresh on submit
+  //enable a messageEvent function to be triggered with EventEmitter, activates the movingroomba
   @Output() messageEvent = new EventEmitter();
 
-  //array of arrays, first array is y 0-9, second is x 0-9
+  //array of arrays, first array is y 0-9, second is x 0-9, used to track button locations
   xArrays = new Array();
 
-  //ideas for the boudnary in visual terms
-  //div filled with x color, rotated to the proper angle, length and height have to be what tells it how long to be
-  //I keep thinking there will be a GUI code or something already made for a line, but IDK
-  //some js code would probably be able to grab the coordinates of each point on the page and input that somehow
-  //how to input that when the lnehgt and hiehgt contreol the size and end points
-  //could have start point - end point = width, then have to get one edge to sit on that first point
-  //if position is based on center, could find halfway between the 2 points to place it at, and the width would be x, y - x, y
-  //something like this
-
+  //saving the previous buttons location
   lastXPoint : number | null;
   lastYPoint : number | null;
 
+  //calculate angle from 0,0 to x,y, returns degrees
   calcAngleDegrees(x:number, y:number) : number{
     return Math.atan2(y, x) * 180 / Math.PI;
   }
 
+  //gets coordinates of element by page instead of by viewport, from javascript.info
+  getCoords(elem : Element) {
+    let box = elem.getBoundingClientRect();
+
+    return {
+      top: box.top + window.pageYOffset,
+      right: box.right + window.pageXOffset,
+      bottom: box.bottom + window.pageYOffset,
+      left: box.left + window.pageXOffset
+    };
+  }
+
+  //create the divs which will create the boundary
   createBoudnaryDiv(theX : number, theY : number) {
     if(this.lastXPoint != null && this.lastYPoint != null){
+      //get button elements that were clicked
       var firstButt = document.getElementById(['butts', this.lastYPoint + 4, ',' , this.lastXPoint + 4].join(''));
       var secondButt = document.getElementById(['butts', theY + 4, ',' , theX + 4].join(''));
+
       if(firstButt != null && secondButt != null){
+        //get x and y to use in calcAngleDegrees
         var xPos = theX - this.lastXPoint;
         var yPos = theY - this.lastYPoint;
-        // get document coordinates of the element
-        function getCoords(elem : Element) {
-          let box = elem.getBoundingClientRect();
 
-          return {
-            top: box.top + window.pageYOffset,
-            right: box.right + window.pageXOffset,
-            bottom: box.bottom + window.pageYOffset,
-            left: box.left + window.pageXOffset
-          };
-        }
-        console.log("First butt x: " + getCoords(firstButt).left + " first butt top: " + getCoords(firstButt).top + " first butt y: " + getCoords(firstButt).top + " get coords second butt y: " + getCoords(secondButt).top + " second butt top: " + getCoords(secondButt).top);
+        //create rotate string to load in style for div
         var rotateString = "rotate(" + parseInt((this.calcAngleDegrees(xPos, yPos)).toString()) + "deg)";
-        console.log(this.calcAngleDegrees(xPos, yPos));
+
+        //create the div segment for the boundary
         var div = document.createElement('div');
         var divStyle = '';
-        var xDist = Math.abs(getCoords(firstButt).left - getCoords(secondButt).left);
-        var yDist = Math.abs(getCoords(firstButt).top - getCoords(secondButt).top);
-        console.log(xDist, "," , yDist);
+
+        //calc total distance between first button and second button in px
+        var xDist = Math.abs(this.getCoords(firstButt).left - this.getCoords(secondButt).left);
+        var yDist = Math.abs(this.getCoords(firstButt).top - this.getCoords(secondButt).top);
+
+        //div width calculated by pythagorean theorem
         var divWidth = Math.sqrt((xDist * xDist)+(yDist * yDist));
-        var theTop = getCoords(firstButt).top;
-        if(getCoords(firstButt).top < getCoords(secondButt).top){
-          theTop = getCoords(firstButt).top;
+
+        //set the distance from the top of the page for the boundary based on which button is higher
+        var theTop = this.getCoords(firstButt).top;
+        if(this.getCoords(firstButt).top < this.getCoords(secondButt).top){
+          theTop = this.getCoords(firstButt).top;
         }
         else {
-          theTop = getCoords(secondButt).top;
+          theTop = this.getCoords(secondButt).top;
         }
-        var theLeft = getCoords(firstButt).left;
-        if(getCoords(firstButt).left < getCoords(secondButt).left){
-          theLeft = getCoords(firstButt).left;
+
+        //set the distance from the left of the page for the boundary based on which button is farther left
+        var theLeft = this.getCoords(firstButt).left;
+        if(this.getCoords(firstButt).left < this.getCoords(secondButt).left){
+          theLeft = this.getCoords(firstButt).left;
         }
         else {
-          theLeft = getCoords(secondButt).left;
+          theLeft = this.getCoords(secondButt).left;
         }
+
+        //add the div to the document and define style
         document.body.append(div);
         div.setAttribute("class", "bound");
         divStyle += (["width:", divWidth, 'px;'].join('').toString());
@@ -77,24 +86,27 @@ export class ThegridComponent implements OnInit {
         divStyle += (['height:', '10px;'].join('').toString());
         divStyle += (['position:', 'absolute;'].join('').toString());
         divStyle += (['background-color:', 'black;'].join('').toString());
-        console.log("Angle: " + this.calcAngleDegrees(xPos, yPos).toString() + " divWidth: " + divWidth + "theLeft: " + theLeft + " TheTop: " + theTop + "The cos: " + Math.cos((this.calcAngleDegrees(xPos, yPos) * (Math.PI/180))));
+        //used law of sines and trigonometry to calc distance from left after rotation from center
         divStyle += (['left:', theLeft - ((divWidth/2) - ((divWidth/2) * Math.abs(Math.cos((this.calcAngleDegrees(xPos, yPos) * (Math.PI/180)))))), 'px;'].join('').toString());
         div.setAttribute('style', divStyle.toString());
+
+        //set the last button clicked to the new first button for the next boundary
         this.lastXPoint = theX;
         this.lastYPoint = theY;
       }
     }
   }
 
-
-
+  //collecting buttons clicked to make boundary for roomba checks
   boudnary = new Array();
 
+  //new button was clicked
   addPoint(xPoint : number, yPoint : number){
     this.boudnary.push([xPoint, yPoint]);
 
     this.messageEvent.emit(null);
 
+    //if this is the first button clicked, save values, else make boundary div
     if(this.lastXPoint == null && this.lastYPoint == null){
       this.lastXPoint = xPoint;
       this.lastYPoint = yPoint;
@@ -105,10 +117,12 @@ export class ThegridComponent implements OnInit {
   }
 
   constructor() {
+    //add x and y coords for buttons
     for (var i = 0; i < 9; i++) {
       this.xArrays[i] = new Array();
       this.xArrays[i].push(-4, -3, -2, -1, 0, 1, 2, 3, 4);
     }
+
     this.lastXPoint = null;
     this.lastYPoint = null;
    }
